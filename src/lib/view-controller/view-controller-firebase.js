8ab7ef7f7d60967e5172ea562cddb26e0d0569e9
dajhelyan <assiaxramx10@gmail.com>
2019-05-29 11:07:46 -0500

    funcion que captura los datos del usuario y ejecuta la funcion register de firebase... Fixed #10

--- src/lib/view-controller/view-controller-firebase.js
+++ src/lib/view-controller/view-controller-firebase.js
@@ -1,5 +1,27 @@
-/* import { login } from '../controller/controller-firebase.js';
+import { register } from '../controller/controller-firebase.js';
+export const changeHash = (hash) => { window.location.hash = hash }
 
-export const logInUser = () => {
+export const registerPage = () => {
+    return changeHash('#/register')
+}
 
-} */
+export const registerUser = () => {
+    /* const nameUser = document.querySelector('#nameUser').value; */
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+ register(email, password)
+ .then(() => {
+     changeHash('#/post');
+ }).catch((error) => {
+     console.log(error);
+ })
+
+ /* .then(objUser => {
+     objUser = {
+         name: nameUser,
+         email: email,
+
+     }
+ }) */
+}
